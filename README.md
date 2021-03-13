2a1d9054e43578b7a0582c03de2723b86f7b78eb
Gabriel Duarte <gabrielmdk01@gmail.com>
2021-03-13 04:57:08 -0300

    Roteiro 2

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # logica-compilador
 
+### EBNF
+
+EXPRESSION = TERM, {("+" | "-"), TERM} ;
+
+TERM = NUMBER, {("*" | "/"), NUMBER} ;
+
+
 ### Diagrama Sintático
 
 ![alt text](https://github.com/gDuarteg/logica-compilador/blob/main/ds.png)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 class Token:
     def __init__(self, _type, _value):
@@ -34,51 +35,84 @@ class Tokenizer:
             self.position += 1
             self.actual = Token('MINUS','-')
         
+        elif self.origin[self.position] == '*':
+            self.position += 1
+            self.actual = Token('MULT','*')
+        
+        elif self.origin[self.position] == '/':
+            self.position += 1
+            self.actual = Token('DIV','/')
+        
         else:
             raise ValueError('Erro')
         return self.actual
     
 class Parser:
     @staticmethod
-    def parseExpression():
+    def term():
         if Parser.tokens.actual.type == "INT":
             result = Parser.tokens.actual.value
             Parser.tokens.selectNext()
             
             if Parser.tokens.actual.type == "INT":
                 raise ValueError('Erro')
+        
+            while Parser.tokens.actual.type == "MULT" or Parser.tokens.actual.type == "DIV":
+                if Parser.tokens.actual.value == "*":
+                    Parser.tokens.selectNext()
+                    if Parser.tokens.actual.type == "INT":
+                        result *= Parser.tokens.actual.value
+                    else:
+                        raise ValueError('Erro')
 
-            while Parser.tokens.actual.type == "PLUS" or Parser.tokens.actual.type == "MINUS":
-                if Parser.tokens.actual.value == "+":
-                    Parser.tokens.selectNext()
-                    
-                    if Parser.tokens.actual.type == "INT":
-                        result += Parser.tokens.actual.value
-                    else:
-                        raise ValueError('Erro')
-                if Parser.tokens.actual.value == "-":
+                if Parser.tokens.actual.value == "/":
                     Parser.tokens.selectNext()
                     if Parser.tokens.actual.type == "INT":
-                        result -= Parser.tokens.actual.value
+                        result //= Parser.tokens.actual.value
                     else:
                         raise ValueError('Erro')
+        else:
+            raise ValueError('Erro')
+        return result
+
+    @staticmethod
+    def parseExpression():
+        
+        result = Parser.term()
+        while Parser.tokens.actual.type == "PLUS" or Parser.tokens.actual.type == "MINUS":
+            if Parser.tokens.actual.value == "+":
                 Parser.tokens.selectNext()
                 
                 if Parser.tokens.actual.type == "INT":
+                    result += Parser.term()
+                else:
                     raise ValueError('Erro')
-            return result
-        else: 
+            if Parser.tokens.actual.value == "-":
+                Parser.tokens.selectNext()
+                if Parser.tokens.actual.type == "INT":
+                    result -= Parser.term()
+                else:
+                    raise ValueError('Erro')
+
+        Parser.tokens.selectNext()
+        if Parser.tokens.actual.type != "EOF": 
             raise ValueError('Erro')
+        return result
 
     @staticmethod
     def run(code):
         Parser.tokens = Tokenizer(code)
         return Parser.parseExpression()
 
+class PrePro:
+    @staticmethod
+    def filter(code):
+        return re.sub(r'/\*.*?\*/',"", code)
 
-def main(argv):
-    return Parser.run(argv[0])
-
+def main(code):
+    code = PrePro.filter(code)
+    print(code)
+    return Parser.run(code)
 
 if __name__ == "__main__":
-   print(main(sys.argv[1:]))
+   print(main(sys.argv[1:][0]))
