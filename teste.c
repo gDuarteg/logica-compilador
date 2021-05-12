2ca83c5043b5975fc0f595f2f35697560d7075c3
gDuarteg <gabrielmdk01@gmail.com>
2021-05-12 06:28:46 -0300

    v2.2

--- teste.c
+++ teste.c
@@ -1,4 +1,29 @@
-a = 2;
-b = 3;
-c = a + b; /*comentario*/
-println(c);
+{
+    z = readln();
+    println(z);
+
+    a = 2;
+    b = 5;
+    {
+        x = 10;
+    }
+    c = a + b; /*comentario*/
+    println(c);
+
+    if (2 == 2) {
+        y = 4;
+    } else {
+        y = 22;
+    }
+    println(y);
+
+    while (a < c || a == 2) {
+        println(a);
+        a = a + 1;
+    }
+
+    while (a < c && !(a == 2)) {
+        println(a);
+        a = a + 1;
+    }
+}
